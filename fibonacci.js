9f91b0db0faba1cebae723728cc1480947d5bc83
pieterei19 <102586217+pieterei19@users.noreply.github.com>
2022-09-13 08:15:27 +0200

    ciag_fibonacciego

--- fibonacci.js
+++ fibonacci.js
@@ -3,4 +3,8 @@ function fibonacci(f){
     if (f == 1) return 1;
     return fibonacci(f-1)+fibonacci(f-2);
 }
-console.log(fibonacci(3));
+i = 0;
+while (i < 10){
+    console.log(fibonacci(i));
+    i++;
+}
